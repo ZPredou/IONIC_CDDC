640162b8649de588d570f14f2bd0e31a5e9d79dd
Decourty William <williamdecourty@live.fr>
2018-03-19 16:02:22 +0100

    added back a plans page of the exposition

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Vibration } from '@ionic-native/vibration';
 import { AboutPage } from '../about/about';
 import { SchoolPage } from '../school/school';
+import { PlansPage } from '../plans/plans';
 import { HomePage } from '../home/home';
 import { SuperTabsModule, SuperTabsController, SuperTabs} from 'ionic2-super-tabs';
 import { Keyboard } from '@ionic-native/keyboard';
@@ -19,6 +20,7 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = AboutPage;
   tab3Root = SchoolPage;
+  tab4Root = PlansPage;
 
   constructor(private vibration: Vibration, private superTabsCtrl: SuperTabsController, public platform: Platform, public keyboard: Keyboard) {
   }
