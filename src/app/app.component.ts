855b71b0b004b7d8eb6c076028dba9ad93bf57cf
Decourty William <williamdecourty@live.fr>
2018-04-04 15:27:51 +0200

    generated splash & icon images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,20 +24,8 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might  need.
       splashScreen.hide();
       statusBar.hide();
-      //this._OneSignal.startInit('b532d545-0594-4d4c-9f34-cc6e3daf1056', '337865225097');
-      //this._OneSignal.inFocusDisplaying(this._OneSignal.OSInFocusDisplayOption.Notification);
-      //this._OneSignal.setSubscription(true);
-      //this._OneSignal.handleNotificationReceived().subscribe(() => {
-        // handle received here how you wish.
-      //});
-      //this._OneSignal.handleNotificationOpened().subscribe(() => {
-        // handle opened here how you wish.
-      //});
-      //this._OneSignal.endInit();
     });
 
     firebase.initializeApp(config);
